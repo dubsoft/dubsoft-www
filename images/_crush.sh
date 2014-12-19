f6ddc8efc45f0d70ab432fc95e702edf3646bd68
wlaurance <w.laurance@gmail.com>
2014-12-19 11:58:17 -0500

    crush manifest

--- images/_crush.sh
+++ images/_crush.sh
@@ -1,6 +1,12 @@
-for image in `ls *.png`
+set1=`ls *.png > set1`
+set2="_crush.manifest"
+images=$(LC_ALL='C' sort -n $set2 $set2 set1 | uniq -u)
+echo $images
+for image in $images
 do
   mv $image old-$image
   pngcrush -reduce -brute old-$image $image
   rm old-$image
+  echo $image >> $set2
 done
+rm set1
