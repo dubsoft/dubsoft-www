61874999fb1cfbf544d166fa29ba73dbfee2f6da
wlaurance <w.laurance@gmail.com>
2015-06-04 11:27:59 -0700

    Add gzip and longer cache

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var static = require('node-static');
 
-var fileServer = new static.Server('./jekyll-site/_site/');
+var fileServer = new static.Server('./jekyll-site/_site/', {
+  gzip: true,
+  cache: 7200,
+});
 require('http').createServer(function (request, response) {
       request.addListener('end', function () {
                 fileServer.serve(request, response);
