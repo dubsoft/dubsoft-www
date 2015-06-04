c3df2de1b7ec84072237bf84b3efc07f965b5b05
wlaurance <w.laurance@gmail.com>
2015-06-04 11:40:03 -0700

    Increase cache time

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var static = require('node-static');
 
 var fileServer = new static.Server('./jekyll-site/_site/', {
   gzip: true,
-  cache: 7200,
+  cache: 72000,
 });
 require('http').createServer(function (request, response) {
       request.addListener('end', function () {
