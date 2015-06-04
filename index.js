431a1148e2112d9ad28603e67c982752be952a17
wlaurance <w.laurance@gmail.com>
2015-06-04 11:41:36 -0700

    make cache even bigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var static = require('node-static');
 
 var fileServer = new static.Server('./jekyll-site/_site/', {
   gzip: true,
-  cache: 72000,
+  cache: 720000,
 });
 require('http').createServer(function (request, response) {
       request.addListener('end', function () {
